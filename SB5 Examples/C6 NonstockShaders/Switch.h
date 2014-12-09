7796d5e5a3681616e63462550e5faa5e6e4efbd8
marblep <marblep@hotmail.com>
2014-12-10 01:39:38 +0800

    Add C6 NonstockShaders

---
+++ SB5 Examples/C6 NonstockShaders/Switch.h
@@ -0,0 +1,11 @@
+//#define ADSGouraud
+//#define ADSPhong
+//#define DiffuseLight
+//#define Dissolve
+//#define FlatShader
+//#define LitTexture
+//#define ProvokingVertex
+//#define ShadedTriangle
+//#define TexturedTriangle
+//#define ToonShader
+#define Triangle
